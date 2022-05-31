6968edb39dc815cfc59454561e6c243251006a23
Lucas Rebato <lrebato10@gmail.com>
2022-05-31 21:00:12 +0100

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import SellixAPI from './SellixAPI';
+
+export default SellixAPI;
+export {
+    SellixApiResponse,
+    SellixCustomer,
+    FraudShield,
+    SellixPaymentCart,
+    GatewayTypes,
+    SellixProduct,
+    SellixPayment,
+    SubscriptionStatus,
+} from './types/API';
+
+export {
+    GetCustomersResponse,
+    GetCustomerResponse,
+    CreateCustomerResponse,
+    UpdateCustomerResponse,
+    CreatePaymentResponse,
+    GetProductResponse,
+    GetProductsResponse,
+    CreateProductResponse,
+    UpdateProductResponse,
+    DeleteProductResponse,
+} from './types/API';
